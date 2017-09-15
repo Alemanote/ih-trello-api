afa5d71781a5e4d615fc717c3c48cb3a6b6ca44f
Carlos del Prado <carlos.delprado@11paths.com>
2017-09-15 20:30:13 +0200

    feat: remove card

--- controllers/cards.controller.js
+++ controllers/cards.controller.js
@@ -16,5 +16,13 @@ module.exports.move = (req, res, next) => {
 }
 
 module.exports.remove = (req, res, next) => {
-    res.status(501).json({ message: 'Unimplemented' });
+    Card.remove({ _id: req.params.id})
+        .then(deleted => {
+            if (deleted.result.n > 0) {
+                res.status(204).json();
+            } else {
+                res.status(404).json({ message: 'Card not found' });
+            }
+        })
+        .catch(err => next(err));
 }
