af91dd7e478078eeaa21075ae4f80b9f07f3b37f
Carlos del Prado <carlos.delprado@11paths.com>
2017-09-15 20:01:49 +0200

    feat: remove list

--- controllers/lists.controller.js
+++ controllers/lists.controller.js
@@ -19,5 +19,14 @@ module.exports.edit = (req, res, next) => {
 }
 
 module.exports.remove = (req, res, next) => {
-    res.status(501).json({ message: 'Unimplemented' });
+    // TODO: validate id as a valid ObjectId 
+    List.remove({_id : req.params.id})
+        .then(removed => {
+            if (removed.result.n > 0) {
+                res.status(204).json()
+            } else {
+                res.status(404).json({ message: 'List not found'})
+            }
+        })
+        .catch(err => next(err));
 }
