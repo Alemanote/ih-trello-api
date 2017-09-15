3e91ce9dac2808f7898ea4b056820b8f69a15c83
Carlos del Prado <carlos.delprado@11paths.com>
2017-09-15 19:35:54 +0200

    feat: get all list

--- controllers/lists.controller.js
+++ controllers/lists.controller.js
@@ -1,7 +1,11 @@
 mongoose = require('mongoose');
+const List = require('../models/list.model');
 
 module.exports.getAll = (req, res, next) => {
-    res.status(501).json({ message: 'Unimplemented' });
+    List.find()
+        .populate('cards')
+        .then(lists => res.json(lists))
+        .catch(err => next(err));
 }
 
 module.exports.create = (req, res, next) => {
