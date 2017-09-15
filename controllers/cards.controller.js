c36546c563023b0321d69cbb4bd358b769a38770
Carlos del Prado <carlos.delprado@11paths.com>
2017-09-15 20:33:57 +0200

    feat: edit card

--- controllers/cards.controller.js
+++ controllers/cards.controller.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
 const Card = require('../models/card.model');
 
 module.exports.create = (req, res, next) => {
@@ -8,7 +9,18 @@ module.exports.create = (req, res, next) => {
 }
 
 module.exports.edit = (req, res, next) => {
-    res.status(501).json({ message: 'Unimplemented' });
+    Card.findById(req.params.id)
+        .then(card => {
+            if (card) {
+                _.merge(card, req.body)
+                card.save()
+                    .then(card => res.status(200).json(card))
+                    .catch(err => next(err));
+            } else {
+                res.status(404).json({ message: 'Card not found' });
+            }
+        })
+        .catch(err => next(err));
 }
 
 module.exports.move = (req, res, next) => {
