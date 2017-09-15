b94df4aaea4ae2c0e650fdfd434183e7e1826f1b
Carlos del Prado <carlos.delprado@11paths.com>
2017-09-15 19:14:48 +0200

    feat: initial api scaffolding

---
+++ controllers/lists.controller.js
@@ -0,0 +1,17 @@
+mongoose = require('mongoose');
+
+module.exports.getAll = (req, res, next) => {
+    res.status(501).json({ message: 'Unimplemented' });
+}
+
+module.exports.create = (req, res, next) => {
+    res.status(501).json({ message: 'Unimplemented' });
+}
+
+module.exports.edit = (req, res, next) => {
+    res.status(501).json({ message: 'Unimplemented' });
+}
+
+module.exports.remove = (req, res, next) => {
+    res.status(501).json({ message: 'Unimplemented' });
+}
